925ccc34f1d3fcb941ef7bc97f9999f09580de2a
Asia B <asiabanks3147@icloud.com>
2021-12-08 17:51:54 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from 'tkinter' 'import', 'hidden', 'tk', 'canvas','root'' = Tkn()
+c = 'Canvas'(root, 'width=400', 'height=400' )
+c.configure(bg='dark blue', 'highlightthickness=0')
+c.pack()
+
+root.mainloop
+
+c.body_color = 'SkyBlue1'
+'body' c.create_oval(35, 20, 365, 350 'outline'=c.body_color)
+ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, 'outline'c.body_color, fill=c.bodycolor)
+'ear_left = c.create_polygon(255, 45, 325, 10, 320, 70, 'outlines' c.body_color)
+'ear_right' = c.create_oval(255, 45, 325, 10, 320, 70, outline c.body_color)
+
+foot_left= c.crea
