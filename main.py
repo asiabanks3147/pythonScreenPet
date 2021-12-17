a212e1c5a0ca7d5d011c4944281280b6e1954b8e
Asia B <asiabanks3147@icloud.com>
2021-12-16 23:17:48 -0600

    project

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from 'tkinter' 'import', 'hidden', 'tk', 'canvas','root'' = Tkn()
-c = 'Canvas'(root, 'width=400', 'height=400' )
-c.configure(bg='dark blue', 'highlightthickness=0')
-c.pack()
+print('Hello, world')
+print("Shelly's friend")
+print(23)
+print(3.14)
 
-root.mainloop
 
-c.body_color = 'SkyBlue1'
-'body' c.create_oval(35, 20, 365, 350 'outline'=c.body_color)
-ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, 'outline'c.body_color, fill=c.bodycolor)
-'ear_left = c.create_polygon(255, 45, 325, 10, 320, 70, 'outlines' c.body_color)
-'ear_right' = c.create_oval(255, 45, 325, 10, 320, 70, outline c.body_color)
+def player_score(args):
+    pass
 
-foot_left= c.crea
+
+print(player_score)
+print('Your final score is', player_score)
+print('Nice to meet you')
