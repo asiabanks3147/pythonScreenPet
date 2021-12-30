a66f8639ddcd7af3f9698f2b898016b8cd8e9d05
Asia B <asiabanks3147@icloud.com>
2021-12-30 12:32:35 -0600

    project

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-print('Hello, world')
-print("Shelly's friend")
-print(23)
-print(3.14)
-
-
-def player_score(args):
-    pass
-
-
-print(player_score)
-print('Your final score is', player_score)
-print('Nice to meet you')
+(print) 350 + 427
+(print)987 - 120
+(print)34 * 45
+(print)57 / 2
+(print)57 // 2
+(print)57 % 2
+(print)3**2
+(printround)(100/3, 2)
+print(100 -5*3)
